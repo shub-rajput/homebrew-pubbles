256ddecc5657d062e44a60a0a728620bb5b1fc38
Shubhang <materealdesigns@gmail.com>
2026-03-04 20:27:28 +0530

    Update cursor-subtitles.rb with post flight safe check

--- Casks/cursor-subtitles.rb
+++ Casks/cursor-subtitles.rb
@@ -1,4 +1,4 @@
- cask "cursor-subtitles" do
+  cask "cursor-subtitles" do
     version "1.0.2"
     sha256 "826cc4099ffe9281aa3b901200251ea3aace26f7a882b9bfe11b754182c0ec84"
 
@@ -8,6 +8,11 @@
     homepage "https://github.com/shub-rajput/cursor_subtitles"
 
     app "CursorSubtitles.app"
+
+    postflight do
+      system_command "/usr/bin/xattr",
+                     args: ["-cr", "#{appdir}/CursorSubtitles.app"]
+    end
 
     zap trash: "~/.config/cursor-subtitles"
   end
