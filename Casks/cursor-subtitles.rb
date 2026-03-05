3cd4d0efd9f042cdf88ddbd8d2a4164632e957c2
Shubhang <materealdesigns@gmail.com>
2026-03-05 07:58:08 +0530

    v1.0.4

--- Casks/cursor-subtitles.rb
+++ Casks/cursor-subtitles.rb
@@ -1,18 +1,18 @@
-  cask "cursor-subtitles" do
-    version "1.0.3"
-    sha256 "c487aed66dfd0a09ecc3f3c9a2c6083fe03f5646bc967e6b7eb05be83890929c"
+  cask "pubbles" do
+    version "1.0.4"
+    sha256 "bf910c8c4f5b5d83c2715465422a19f3a8b957ca7959c72353719981cc5df1c7"
 
-    url "https://github.com/shub-rajput/cursor_subtitles/releases/download/v#{version}/CursorSubtitles_v#{version}.zip"
-    name "CursorSubtitles"
-    desc "Real-time on-screen subtitles that follow your cursor"
-    homepage "https://github.com/shub-rajput/cursor_subtitles"
+    url "https://github.com/shub-rajput/pubbles/releases/download/v#{version}/Pubbles_v#{version}.zip"
+    name "Pubbles"
+    desc "Subtitle bubbles for your pointer"
+    homepage "https://github.com/shub-rajput/pubbles"
 
-    app "CursorSubtitles.app"
+    app "Pubbles.app"
 
     postflight do
       system_command "/usr/bin/xattr",
-                     args: ["-cr", "#{appdir}/CursorSubtitles.app"]
+                     args: ["-cr", "#{appdir}/Pubbles.app"]
     end
 
-    zap trash: "~/.config/cursor-subtitles"
+    zap trash: "~/.config/pubbles"
   end
